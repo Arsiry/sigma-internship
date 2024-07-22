243652adc656d733e5f581b13f5fb3c143e043b2
Arsiry <a-arsiry@ukr.net>
2024-07-22 10:24:18 -0400

    python programming

---
+++ python-bootcamp/day1.py
@@ -0,0 +1,19 @@
+#print("Hello World!)")
+
+#print("Hello World!)")
+#print("Hello World!)")
+#print("Hello World!)")
+
+#print("Hello World!\nHello World!\nHello World!")
+
+#print("Hello " + "Anastasiia")
+#print("Hello" + " " + "Anastasiia")
+
+#input("What is your name \n")
+
+#print("Hello " + input("What is your name\n"))
+
+name = input("What is your name\n")
+print("Hello " + name)
+length = len(name)
+print(length)
